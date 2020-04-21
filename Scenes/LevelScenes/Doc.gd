b3300cf6b5d77a08586962d67626513fcb7a8fa0
ZhongXiLu <zhongxilu@hotmail.com>
2020-04-21 20:10:48 +0200

    Refactored boss attacks + added more samples

--- Scenes/LevelScenes/Doc.gd
+++ Scenes/LevelScenes/Doc.gd
@@ -1,46 +1,28 @@
 extends KinematicBody2D
 
-export var initial_bullet_speed = 50
-export var bullet_speed = 2500
-export var fire_rate = 2
+export(Array, Resource) var attacks = []
 
-var bullet = preload("res://Scenes/ObjectScenes/CD.tscn")
-var can_fire = true
+export var attack_rate = 5
+
+var can_attack = false
 var dead = false
 
 
-func _process(delta):
-    if can_fire and !dead:
-        can_fire = false
-        
-        var bullet_instances = []
-        
-        # Where to instantiate the bullets => outside the sprite
-        var sprite_size_offset = $AnimatedSprite.frames.get_frame("idle", 0).get_width() * 1.2
-        
-        # Create 8 bullets in each direction + set initial speed
-        var directions = [0, -1, 1]
-        for direction in directions:
-            for direction2 in directions:
-                if direction != 0 or direction2 != 0:
-                    var bullet_instance = bullet.instance()
-                    bullet_instance.position.x = global_position.x + (direction * sprite_size_offset)
-                    bullet_instance.position.y = global_position.y + (direction2 * sprite_size_offset)
-                    get_parent().add_child(bullet_instance)
-                    bullet_instance.set_linear_velocity(Vector2(direction * initial_bullet_speed, direction2 * initial_bullet_speed))
-                    bullet_instances.append(bullet_instance)
-            
-        # Delay the attack a little bit, so the player has time to react
-        yield(get_tree().create_timer(1), "timeout")
-    
-        # Speed the bullet up after delay
-        var speed_factor = bullet_speed / initial_bullet_speed
-        for bullet_instance in bullet_instances:
-            bullet_instance.linear_velocity.x *= speed_factor
-            bullet_instance.linear_velocity.y *= speed_factor
+func _ready():
+    $Introduction.play()
+    yield(get_tree().create_timer(5), "timeout")
+    can_attack = true
 
-        yield(get_tree().create_timer(fire_rate), "timeout")
-        can_fire = true
+
+func _process(delta):
+    if can_attack and !dead:
+        can_attack = false
+
+        if attacks:
+            add_child(attacks[0].instance())
+
+        yield(get_tree().create_timer(attack_rate), "timeout")
+        can_attack = true
 
 
 func _physics_process(_delta):
---
+++ Scripts/Attacks/Doc/Attack1.gd
@@ -0,0 +1,39 @@
+extends Node
+
+export var initial_bullet_speed = 50
+export var bullet_speed = 2500
+
+var bullet = preload("res://Scenes/ObjectScenes/CD.tscn")
+var bullet_instances = []
+
+func _ready():
+    
+    $AudioStreamPlayer.play()
+    
+    # Where to instantiate the bullets => outside the sprite
+    var sprite_size_offset = get_parent().get_node("AnimatedSprite").frames.get_frame("idle", 0).get_width() * 1.2
+    
+    # Create 8 bullets in each direction + set initial speed
+    var directions = [0, -1, 1]
+    for direction in directions:
+        for direction2 in directions:
+            if direction != 0 or direction2 != 0:
+                var bullet_instance = bullet.instance()
+                bullet_instance.position.x = get_parent().global_position.x + (direction * sprite_size_offset)
+                bullet_instance.position.y = get_parent().global_position.y + (direction2 * sprite_size_offset)
+                add_child(bullet_instance)
+                bullet_instance.set_linear_velocity(Vector2(direction * initial_bullet_speed, direction2 * initial_bullet_speed))
+                bullet_instances.append(bullet_instance)
+        
+    # Delay the attack a little bit, so the player has time to react
+    yield(get_tree().create_timer(1), "timeout")
+
+    # Speed the bullet up after delay
+    var speed_factor = bullet_speed / initial_bullet_speed
+    for bullet_instance in bullet_instances:
+        bullet_instance.linear_velocity.x *= speed_factor
+        bullet_instance.linear_velocity.y *= speed_factor
+
+func _process(_delta):
+    if bullet_instances.empty():
+        queue_free()        # attack is over

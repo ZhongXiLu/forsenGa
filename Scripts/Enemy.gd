09e974c420b9210f56a7b900570df5c11a016313
ZhongXiLu <zhongxilu@hotmail.com>
2020-05-04 22:25:39 +0200

    Added basic enemy

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+export var gravity = 60
+export var speed = 600
+
+var motion = Vector2()
+
+func _physics_process(_delta):
+    
+    if !$FloorEdge.is_colliding() or $WallEdge.is_colliding():
+        print(name, " is colliding")
+        scale.x = -1
+        speed *= -1
+
+    motion.y += gravity
+    motion.x = speed
+        
+    motion = move_and_slide_with_snap(motion, Vector2(0, -1))
+
+
+func _on_Area2D_body_entered(body):
+    if body.get_collision_layer_bit(1):     # player
+        body.get_node("Stats").health -= 1
+
+
+func _on_Stats_no_health():
+    queue_free()
